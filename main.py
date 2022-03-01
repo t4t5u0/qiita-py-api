6aedf5bfef5b4403e48c004b81316e2f533545c1
t4t5u0 <ymmtryk0902@gmail.com>
2022-03-02 03:23:47 +0900

    Update: poetryで動くようにした

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import uvicorn
 from fastapi import FastAPI
 
-from libs.getArticle import Article, getArticle
+
+from qiita_py_sdk.getArticle import getArticle, Article
+
 
 app = FastAPI()
+
 
 @app.get("/v1/qiita/{user_id}")
 async def get_articles(user_id: str) -> list[Article]:
     return getArticle(user_id)
 
+
 def main():
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
 
 if __name__ == "__main__":
     main()
